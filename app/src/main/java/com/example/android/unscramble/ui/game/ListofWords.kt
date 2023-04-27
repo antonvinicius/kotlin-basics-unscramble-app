c2799ef0cdcd0120cf9cabd66e415aa3d40db913
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-04-26 21:40:27 -0300

    feat: Add view model combined with live data and data binding

--- app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -16,11 +16,9 @@
 
 package com.example.android.unscramble.ui.game
 
-
 const val MAX_NO_OF_WORDS = 10
 const val SCORE_INCREASE = 20
 
-// List with all the words for the Game
 val allWordsList: List<String> =
     listOf("animal",
         "auto",
